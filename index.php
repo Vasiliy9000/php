fc596112e360205457f968426dd80144beb486f5
Vasiliy9000 <60994491+Vasiliy9000@users.noreply.github.com>
2022-08-13 03:38:00 +0300

    Создание .txt
    Создаёт файл и записывает в нем данные

--- index.php
+++ index.php
@@ -2,26 +2,23 @@
 <html> 
     <head> 
         <meta charset="utf-8"> 
-        <title>Отправится письмо</title> 
+        <title> Запишется в файл hello.txt </title> 
     </head> 
 <body> 
     <form action="index.php" method="POST"> 
-        <label for="fname">Отправится письмо на указанную почту</label><br> 
-        <input type="text" name="$to"  value="mr.nightexpres@gmail.com" ><br> 
-        <input type="text" name="$subject"  value="Тема письма" ><br> 
-        <input type="text" name="$message"  value="Само письмо" ><br>
-        <input type="submit" value="Отправить"> 
+        <label for="fname"> Запишется в файл hello.txt</label><br> 
+        <input type="text" id="fname" name="fname" ><br> 
+        <input type="submit" value="Записать"> 
     </form> 
-    <?php // из супер переменной берем знчения
-        if (isset($_POST['$to'],$_POST['$subject'],$_POST['$message'])) 
-        {
-            $to = $_POST['$to'];
-            $subject = $_POST['$subject'];
-            $message = $_POST['$message'];
-            mail($to, $subject, $message);
-
-        }
-    ?>
-
+    <?php 
+        if (isset($_POST['fname'])) 
+        { 
+            $str = $_POST['fname']; 
+            $fd = fopen("hello.txt", 'w') or die("не удалось создать файл"); 
+            fwrite($fd, $str); 
+            fclose($fd); 
+            echo "$str"; 
+        } 
+    ?> 
 </body> 
-</html>
+</html> 
